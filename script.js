ce18a49d8df379d65ee38decd7610b23d5d28eaf
DavidParedesV20 <145064481+DavidParedesV20@users.noreply.github.com>
2025-01-19 20:24:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Archivo script.js
+document.getElementById("transformButton").addEventListener("click", () => {
+    const input = document.getElementById("wordInput").value;
+    let transformedWord = "";
+
+    // Remover acentos del texto de entrada
+    const normalizedInput = input.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+
+    if (normalizedInput.length < 5) {
+        transformedWord = `P@gin@${normalizedInput.replace(/a/gi, "@").replace(/o/gi, "0")}00`;
+    } else {
+        const firstChar = normalizedInput[0];
+        let restOfWord = normalizedInput.slice(1)
+            .replace(/a/gi, "@")
+            .replace(/o/gi, "0");
+        transformedWord = firstChar.toUpperCase() + restOfWord + "00";
+    }
+
+    document.getElementById("result").textContent = transformedWord.replace(/\s+/g, "");
+});
+
+document.getElementById("copyButton").addEventListener("click", () => {
+    const resultText = document.getElementById("result").textContent;
+    if (resultText) {
+        navigator.clipboard.writeText(resultText).then(() => {
+            alert("Palabra copiada al portapapeles!");
+        }).catch(err => {
+            alert("Hubo un error al copiar la palabra.");
+        });
+    } else {
+        alert("No hay palabra generada para copiar.");
+    }
+});
